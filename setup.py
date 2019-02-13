dcbcbe9383fbae08d3849eb668da1e2eb1e43dc8
Jaseibert <Jaseibert5@gmail.com>
2019-02-12 22:28:54 -0600

    12th Commit - Added Requirements.txt and Versioning in Setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ INSTALL_REQUIRES = (
 
 setuptools.setup(
     name='Federal',
-    version='0.0.2',
+    version='0.1.0',
     author='Jeremy A. Seibert',
     author_email='Jaseibert2@eagles.usi.edu',
     description="A wrapper on to the pandas-datareader package for easier handling of federal reserve (FRED) data",
